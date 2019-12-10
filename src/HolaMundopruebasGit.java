504f5687ef369d2b3b93a3d7d45894df7f63a37c
roquepb <roque.pelaez@yahoo.es>
2019-12-10 11:38:43 +0100

    Este es el seguundo comit-he agregado la seguna syso

--- src/HolaMundopruebasGit.java
+++ src/HolaMundopruebasGit.java
@@ -4,6 +4,9 @@ public class HolaMundopruebasGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una prueba de hola mundo en eclipse para sincronizar con git");
+		
+		
+		System.out.println("Esto es una prueba para el segundo comit desse eclipse");
 	}
 
 }
