8f9cc59a85ea782e00d8f14f8931aa686a2759b8
roquepb <roque.pelaez@yahoo.es>
2019-12-10 11:37:25 +0100

    Este es el primer commit desde eclipse. he creado la clase holamundogit

---
+++ src/HolaMundopruebasGit.java
@@ -0,0 +1,9 @@
+
+public class HolaMundopruebasGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una prueba de hola mundo en eclipse para sincronizar con git");
+	}
+
+}
